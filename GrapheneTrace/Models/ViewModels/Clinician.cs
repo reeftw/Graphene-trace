ef268b0ea7d932c25db86358f64136d64c6250a7
Anakha Raj Thampithara Jayaraj <anakharajthampitharajayaraj@Anakhas-MacBook-Air-2.local>
2025-10-28 16:44:36 +0000

    ui created for patient

---
+++ GrapheneTrace/Models/ViewModels/Clinician.cs
@@ -0,0 +1,8 @@
+namespace GrapheneTrace.Models
+{
+    public class Clinician
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Specialization { get; set; } = string.Empty;
+    }
+}
